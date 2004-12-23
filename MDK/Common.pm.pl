4d6379b3437f459d3afec0a13dd80cd40f12307d
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-12-23 18:19:58 +0000

    1.1.21
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/perl-MDK-Common/trunk@215921 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- MDK/Common.pm.pl
+++ MDK/Common.pm.pl
@@ -74,7 +74,7 @@ use vars qw(@ISA @EXPORT $VERSION); #);
 # perl_checker: RE-EXPORT-ALL
 @EXPORT = map { @$_ } map { values %{'MDK::Common::' . $_ . 'EXPORT_TAGS'} } grep { /::$/ } keys %MDK::Common::;
 
-$VERSION = "1.1.20";
+$VERSION = "1.1.21";
 
 1;
 EOF
