4de4b6002516ba967e9a75cebbb3ebc122012d25
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-11-15 16:00:42 +0000

    inline unneeded cat_a_file() in cat_(), simplify using map (perl_checker could have detected this push/map like structure)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/perl-MDK-Common/trunk@215939 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- MDK/Common/File.pm
+++ MDK/Common/File.pm
@@ -132,8 +132,7 @@ our %EXPORT_TAGS = (all => [ @EXPORT_OK ]);
 
 sub dirname { local $_ = shift; s|[^/]*/*\s*$||; s|(.)/*$|$1|; $_ || '.' }
 sub basename { local $_ = shift; s|/*\s*$||; s|.*/||; $_ }
-sub cat_a_file { open(my $F, $_[0]) or return; my @l = <$F>; @l }
-sub cat_ { my @l; @l = (@l, cat_a_file($_)) foreach @_; wantarray() ? @l : join '', @l }
+sub cat_ { my @l = map { my $F; open($F, $_) ? <$F> : () } @_; wantarray() ? @l : join '', @l }
 sub cat_or_die { open(my $F, $_[0]) or die "can't read file $_[0]: $!\n"; my @l = <$F>; wantarray() ? @l : join '', @l }
 sub cat__ { my ($f) = @_; my @l = <$f>; wantarray() ? @l : join '', @l }
 sub output { my $f = shift; open(my $F, ">$f") or die "output in file $f failed: $!\n"; print $F $_ foreach @_; 1 }
