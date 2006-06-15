ac18c871261d51a32965fb5ba6e08e25580d55a1
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2006-06-15 08:20:08 +0000

    add a fake packdrake.pm
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/perl-MDK-Common/trunk@215973 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ perl_checker_fake_packages/packdrake.pm
@@ -0,0 +1,25 @@
+package packdrake;
+
+sub new {
+    my ($_class, $_file, %_options) = @_;
+}
+
+sub extract_archive {
+    my ($_pack, $_dir, @_files) = @_;
+}
+
+sub extract_all_archive {
+    my ($_pack, $_dir) = @_;
+}
+
+sub list_archive {
+    my (@_files) = @_;
+}
+
+sub build_archive {
+    my ($_listh, $_dir, $_archive, $_size, $_compress, $_uncompress) = @_;
+}
+
+sub cat_archive {
+    my (@_files) = @_;
+}
