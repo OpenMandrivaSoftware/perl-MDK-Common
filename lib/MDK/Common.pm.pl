0b589cfa88986973bc8805c41122cd790e822853
pixel <pixel@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-01-18 15:18:13 +0000

    1.2.10
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/perl-MDK-Common/trunk@233075 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lib/MDK/Common.pm.pl
+++ lib/MDK/Common.pm.pl
@@ -73,7 +73,7 @@ our @ISA = qw(Exporter);
 # perl_checker: RE-EXPORT-ALL
 our @EXPORT = map { @$_ } map { values %{'MDK::Common::' . $_ . 'EXPORT_TAGS'} } grep { /::$/ } keys %MDK::Common::;
 
-our $VERSION = "1.2.9";
+our $VERSION = "1.2.10";
 
 1;
 EOF
