8c22b0ead9ab4bb9445e480f20dddd0bd97a4e56
pterjan <pterjan@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-01-26 09:50:06 +0000

    protect parenthesis in setVarsInSh and drop dead code (#47298)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/perl-MDK-Common/trunk@252135 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lib/MDK/Common/System.pm
+++ lib/MDK/Common/System.pm
@@ -377,8 +377,7 @@ sub setVarsInShMode {
 	    if ($val =~ /["`\$]/) {
 		$val =~ s/(')/\\$1/g;
 		$val = qq('$val');
-	    } elsif ($val =~ /['|\s\\]/) {
-		$val =~ s/(["\\])/\\$1/g;
+	    } elsif ($val =~ /[\(\)'|\s\\]/) {
 		$val = qq("$val");
 	    }
 	    "$_=$val\n";
