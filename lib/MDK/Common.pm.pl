7efe01fa8abb44e895de4d26ade7fb52fa2821d3
eugeni <eugeni@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2011-02-02 15:44:42 +0000

    Revert previous commit, easier to do it elsewhere.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/perl-MDK-Common/trunk@271868 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lib/MDK/Common.pm.pl
+++ lib/MDK/Common.pm.pl
@@ -73,7 +73,7 @@ our @ISA = qw(Exporter);
 # perl_checker: RE-EXPORT-ALL
 our @EXPORT = map { @$_ } map { values %{'MDK::Common::' . $_ . 'EXPORT_TAGS'} } grep { /::$/ } keys %MDK::Common::;
 
-our $VERSION = "1.2.27";
+our $VERSION = "1.2.26.1";
 
 1;
 EOF
